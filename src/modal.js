5df28520e196563281eb17034f1b844e9f5b7130
PanKal1995 <107562957+PanKal1995@users.noreply.github.com>
2022-10-04 12:11:48 +0300

    Ice-cream-project-complete
    Modal_windows

---
+++ src/modal.js
@@ -0,0 +1,30 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open]"),
+      closeModalBtn: document.querySelector("[data-modal-close]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
+
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-location-open]"),
+      closeModalBtn: document.querySelector("[data-location-close]"),
+      modal: document.querySelector("[data-location]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-close");
+    }
+  })();
